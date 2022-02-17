ccf4f5ead4d9501d2bb7296b59b6d1e766277a20
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-02-17 15:44:44 -0300

    10.1 final

---
+++ modulo_2/10.1/first-react/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HellowWorld extends React.Component {
+  render(){
+    return(
+      <>
+      <h1 className='hello' >Hello World</h1>
+      <p>testando primeiro app react</p>
+      </>
+    )
+  }
+}
+
+export default HellowWorld;
